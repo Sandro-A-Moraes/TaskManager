3a2d1914b90ff74e600742ccf2c00dd5d80b3c65
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-03-22 22:55:50 -0300

    feat: add refresh functionality to ListingPage with loading state management

--- src/modules/tasks/pages/ListingPage.tsx
+++ src/modules/tasks/pages/ListingPage.tsx
@@ -1,9 +1,20 @@
 import StatCard from "../ui/StatCard";
 import TaskList from "../components/TaskList";
 import { useTasksContext } from "../context/TaskContext";
+import { useTasks } from "../hooks/useTasks";
+import { useState } from "react";
 
 const ListingPage = () => {
   const { tasksList } = useTasksContext();
+  const { refreshTasks, loading } = useTasks();
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const handleRefresh = async () => {
+    setIsRefreshing(true);
+    await refreshTasks();
+    setIsRefreshing(false);
+  };
+
   const total = tasksList.length;
   const pendentes = tasksList.filter(
     (task) => task.status === "pendente",
@@ -17,11 +28,27 @@ const ListingPage = () => {
 
   return (
     <div className="p-6">
-      <div className="my-3 flex flex-col gap-2">
-        <h1>Suas Tarefas</h1>
-        <p className="font-extralight">
-          Organize e acompanhe o progresso de suas atividades
-        </p>
+      <div className="my-3 flex flex-col gap-2 justify-between">
+        <div className="flex items-center justify-between">
+          <div className="w-3/4">
+            <h1>Suas Tarefas</h1>
+            <p className="font-extralight">
+              Organize e acompanhe o progresso de suas atividades
+            </p>
+          </div>
+          <button
+            onClick={handleRefresh}
+            disabled={isRefreshing || loading}
+            className="group p-3 rounded-lg  bg-purple-100 hover:bg-purple-200 transition-colors disabled:opacity-50"
+            title="Recarregar lista"
+          >
+            <i
+              className={`fa-solid fa-arrows-rotate text-purple-600 ${
+                isRefreshing ? "animate-spin" : "group-hover:rotate-180"
+              } transition-transform duration-300`}
+            />
+          </button>
+        </div>
       </div>
 
       <div className="flex flex-col my-8 gap-4">
